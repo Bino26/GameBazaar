d5db8c3bc219b8fdc8233c00f3f62e1c26cca460
Bino26 <hbino1999@gmail.com>
2024-06-04 23:57:56 +0000

    🧹Refactor Code - Home.razor

---
+++ GameBazaar.Client/Clients/GamesClient.cs
@@ -0,0 +1,41 @@
+﻿using SharedLibrary.Models;
+
+namespace GameBazaar.Client.Clients
+{
+    public class GamesClient
+    {
+        private readonly List<Game> games = new()
+            {
+        new Game()
+        {
+            Id = 1 ,
+            Name = "Street Fighter II",
+            Genre ="Fighting",
+            Price = 19.99M,
+            ReleaseDate = new DateTime(1999,2,1)
+
+        },
+         new Game()
+        {
+            Id = 2 ,
+            Name = "Final Fantasy XIV",
+            Genre ="Roleplaying",
+            Price = 59.99M,
+            ReleaseDate = new DateTime(2010,9,30)
+
+
+        },
+        new Game()
+        {
+            Id = 3 ,
+            Name = "FIFA 23 ",
+            Genre ="Sports",
+            Price = 69.99M,
+            ReleaseDate = new DateTime(2022,9,27)
+
+        },
+    };
+
+        public Game[] GetGames() => [.. games];
+    }
+}
